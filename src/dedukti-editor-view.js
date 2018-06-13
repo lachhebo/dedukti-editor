4e02038cef9e39d44bde863892d0cac345230a76
Ismaël Lachheb <ismaellachheb@gmail.com>
2018-06-13 15:40:50 +0200

    definitive fix for the file issue, code refactored

--- src/dedukti-editor-view.js
+++ src/dedukti-editor-view.js
@@ -1,53 +1,49 @@
-
 class DeduktiEditorView {
-
   constructor() {
+    // It's just a simple web page created using the DOM API.
 
-        // It's just a simple web page created using the DOM API.
+    // Create root element
+    this.element = document.createElement("div");
+    this.element.classList.add("dedukti-editor");
 
-       // Create root element
-       this.element = document.createElement('div');
-       this.element.classList.add('dedukti-editor');
+    //First title
+    this.proof = document.createElement("h2");
+    this.proof.textContent = "Goals";
+    this.element.appendChild(this.proof);
+    this.proof.classList.add("highlight");
+    this.proof.style.textAlign = "center";
 
-       //First title
-        this.proof = document.createElement('h2');
-        this.proof.textContent = 'Goals';
-        this.element.appendChild(this.proof);
-        this.proof.classList.add("highlight");
-        this.proof.style.textAlign = "center";
+    //The tree
+    this.list_of_proof = document.createElement("ol");
+    this.element.appendChild(this.list_of_proof);
+    this.list_of_proof.classList.add("list-tree");
+    this.list_of_proof.setAttribute("id", "proof-tree");
 
-        //The tree
-        this.list_of_proof = document.createElement('ol');
-        this.element.appendChild(this.list_of_proof);
-        this.list_of_proof.classList.add("list-tree");
-        this.list_of_proof.setAttribute("id", "proof-tree");
+    //Second Title
+    this.focus = document.createElement("h2");
+    this.focus.textContent = "Focus";
+    this.element.appendChild(this.focus);
+    this.focus.classList.add("highlight");
+    this.focus.style.textAlign = "center";
 
-        //Second Title
-        this.focus = document.createElement('h2');
-        this.focus.textContent = 'Focus';
-        this.element.appendChild(this.focus);
-        this.focus.classList.add("highlight");
-        this.focus.style.textAlign = "center";
+    //List of Hypothesis :
+    this.list_of_hypothesis = document.createElement("ol");
+    this.element.appendChild(this.list_of_hypothesis);
+    this.list_of_hypothesis.classList.add("list-group");
 
-        //List of Hypothesis :
-        this.list_of_hypothesis = document.createElement('ol');
-        this.element.appendChild(this.list_of_hypothesis);
-        this.list_of_hypothesis.classList.add("list-group");
+    //bar
+    this.bar = document.createElement("hr");
+    this.element.appendChild(this.bar);
 
-        //bar
-        this.bar = document.createElement('hr')
-        this.element.appendChild(this.bar);
+    //Current objective
+    this.current_objective = document.createElement("span");
+    this.element.appendChild(this.current_objective);
+    this.current_objective.classList.add("icon", "icon-microscope");
 
-        //Current objective
-        this.current_objective = document.createElement('span');
-        this.element.appendChild(this.current_objective);
-        this.current_objective.classList.add('icon','icon-microscope');
+    // The array we need to store the element we are going to create :
 
-
-        // The array we need to store the element we are going to create :
-
-        //The style modification we implement :
-        //console.log(this.proof.style);
+    //The style modification we implement :
+    //console.log(this.proof.style);
 
     /*  this.proof.style.paddingLeft = '40%';
         this.proof.style.paddingRight = '50%';
@@ -61,11 +57,12 @@ class DeduktiEditorView {
     */
   }
 
-  initialise_exemple(){
-
-
-    this.updateHypothesis(["exemple de rendu : goal 1","exemple de rendu : goal 1","exemple de rendu : goal 1"]);
-
+  initialise_exemple() {
+    this.updateHypothesis([
+      "exemple de rendu : goal 1",
+      "exemple de rendu : goal 1",
+      "exemple de rendu : goal 1"
+    ]);
   }
 
   // Returns an object that can be retrieved when package is activated
@@ -73,31 +70,28 @@ class DeduktiEditorView {
 
   // Tear down any state and detach
   destroy() {
-  this.element.remove();
+    this.element.remove();
   }
 
-  updateView(){
-  }
+  updateView() {}
 
-  updateSubProof(){
-
+  updateSubProof() {
     let j;
-    var new_list_of_proof = document.createElement('ol');
+    var new_list_of_proof = document.createElement("ol");
 
-    for(j=0;j<this.data_proof_array;j++){
-
-      var souspreuvre =  document.createElement('li');
+    for (j = 0; j < this.data_proof_array; j++) {
+      var souspreuvre = document.createElement("li");
       souspreuvre.classList.add("list-nested-item");
       souspreuvre.setAttribute("id", this.data_proof_array[i].id);
 
       // On crée le sous arbre
-      var sousarbre =  document.createElement('ul');
-      sousarbre.classList.add("entries","list-tree" );
+      var sousarbre = document.createElement("ul");
+      sousarbre.classList.add("entries", "list-tree");
 
-      var div =  document.createElement('div');
-      div.classList.add("header","list-item");
+      var div = document.createElement("div");
+      div.classList.add("header", "list-item");
 
-      var span = document.createElement('span');
+      var span = document.createElement("span");
       span.innerText = this.data_proof_array[i].name;
       div.appendChild(span);
 
@@ -105,44 +99,48 @@ class DeduktiEditorView {
       souspreuvre.appendChild(div);
       souspreuvre.appendChild(sousarbre);
 
-      if(this.data_proof_array[i].parent_id === 0){
+      if (this.data_proof_array[i].parent_id === 0) {
         new_list_of_proof.appendChild(souspreuvre);
-        span.classList.add("icon","icon-key");
-      }
-      else{
-        span.classList.add("icon","icon-link");
-        let parent_tree = document.getElementById(this.data_proof_array[i].parent_id);
+        span.classList.add("icon", "icon-key");
+      } else {
+        span.classList.add("icon", "icon-link");
+        let parent_tree = document.getElementById(
+          this.data_proof_array[i].parent_id
+        );
         let children = parent_tree.childNodes;
         let list_tree = children.item(1);
         list_tree.appendChild(souspreuvre);
       }
     }
 
-    this.list_of_proof = this.element.replaceChild(new_list_of_proof, this.list_of_proof);
+    this.list_of_proof = this.element.replaceChild(
+      new_list_of_proof,
+      this.list_of_proof
+    );
   }
 
-  updateHypothesis(hypothesis_array){
-
-    let new_list_of_hypothesis = document.createElement('ol');
+  updateHypothesis(hypothesis_array) {
+    let new_list_of_hypothesis = document.createElement("ol");
 
     let i;
-    for(i=0;i<hypothesis_array.length;i++){
-
-      var hypothesis =  document.createElement('li');
+    for (i = 0; i < hypothesis_array.length; i++) {
+      var hypothesis = document.createElement("li");
       hypothesis.classList.add("list-item");
 
-      var span = document.createElement('span');
-      span.classList.add('icon','icon-bookmark');
+      var span = document.createElement("span");
+      span.classList.add("icon", "icon-bookmark");
       span.innerText = hypothesis_array[i];
       hypothesis.appendChild(span);
       new_list_of_hypothesis.append(hypothesis);
     }
 
-    this.list_of_hypothesis = this.element.replaceChild(new_list_of_hypothesis, this.list_of_hypothesis);
+    this.list_of_hypothesis = this.element.replaceChild(
+      new_list_of_hypothesis,
+      this.list_of_hypothesis
+    );
   }
 
-  cursor_tree_update(id){
-
+  cursor_tree_update(id) {
     let to_colorize = document.getElementById(id);
 
     let children = to_colorize.childNodes;
@@ -151,38 +149,35 @@ class DeduktiEditorView {
 
     span.classList.remove("icon-key", "icon-link");
     span.classList.add("icon-eye-watch");
-
   }
 
-  setCurrentObjectif(current){
+  setCurrentObjectif(current) {
     this.current_objective.innerText = current;
   }
 
-  set_data_array(data_server){
+  set_data_array(data_server) {
     this.data_proof_array = data_server;
   }
 
   getElement() {
-  return this.element;
+    return this.element;
   }
 
-  getTitle(){
-  return 'Proof Assistant'; // Title of the Information Panel
+  getTitle() {
+    return "Proof Assistant"; // Title of the Information Panel
   }
 
-  getURI(){
-  return 'atom://active-editor-info';
+  getURI() {
+    return "atom://active-editor-info";
   }
 
-  getDefaultLocation(){
-  return 'right'; //Position of the panel
+  getDefaultLocation() {
+    return "right"; //Position of the panel
   }
 
-  getAllowedLocation(){
-  return ['left','right','bottom']; //Where we can move it.
+  getAllowedLocation() {
+    return ["left", "right", "bottom"]; //Where we can move it.
   }
-
 }
-
 
 exports.default = DeduktiEditorView;
--- src/main.js
+++ src/main.js
@@ -1,18 +1,26 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 
 const dk = require("./dedukti-editor-view");
 const URL = require("url");
 const child_process = require("child_process");
-const {AutoLanguageClient, DownloadFile} = require("atom-languageclient");
+const { AutoLanguageClient, DownloadFile } = require("atom-languageclient");
 
 class DeduktiLanguageClient extends AutoLanguageClient {
-
-  constructor () {   /* At the opening of Atom */
+  constructor() {
+    /* At the opening of Atom */
     super();
 
-    atom.config.set("core.debugLSP", true);    // Debug by default;
-    this.config = require("./config.json");  // To modify the configuration, check the setting view
-
+    atom.config.set("core.debugLSP", true); // Debug by default;
+    this.config = require("./config.json"); // To modify the configuration, check the setting view
+    /*
     //New keybindings:
     atom.commands.add("atom-workspace",
       {"dedukti-editor:command1": () => this.command1()})
@@ -20,65 +28,90 @@ class DeduktiLanguageClient extends AutoLanguageClient {
       {"dedukti-editor:command2": () => this.command2()})
     atom.commands.add("atom-workspace",
       {"dedukti-editor:command3": () => this.command3()})
-
+    */
     this.deduktiEditorView = new dk.default(); // We create the view
     //this.deduktiEditorView.initialise_exemple();
-    this._disposable.add(atom.workspace.observeActiveTextEditor((editor) => {
-        if(typeof editor != 'undefined'){ //In case the pane is not a file (like a setting view)
-          let scopeName = editor.getGrammar().scopeName
-          if(this.getGrammarScopes().includes(scopeName)){
-            this._disposable.add(editor.onDidChangeCursorPosition( (cursor) => {
+    this._disposable.add(
+      atom.workspace.observeActiveTextEditor(editor => {
+        if (typeof editor != "undefined") {
+          //In case the pane is not a file (like a setting view)
+          let scopeName = editor.getGrammar().scopeName;
+          if (this.getGrammarScopes().includes(scopeName)) {
+            this._disposable.add(
+              editor.onDidChangeCursorPosition(cursor => {
                 //this.deduktiEditorView.updateView(cursor);
-            }));
+              })
+            );
             atom.workspace.open(this.deduktiEditorView);
-          }
-          else{
+          } else {
             atom.workspace.hide(this.deduktiEditorView);
           }
-        }
-        else{
+        } else {
           atom.workspace.hide(this.deduktiEditorView);
         }
-    }));
-  };
-
+      })
+    );
+  }
 
   startServer(projectPath) {
-     const homedir = require('os').homedir();
-     return super.startServer(homedir);
+    const homedir = require("os").homedir();
+    return super.startServer(homedir);
   }
 
-  shouldStartForEditor(editor) {
-      console.log("on regarde si on doit démarrer le server pour cet éditeur");
-      console.log(editor.getGrammar().scopeName);
-      return this.getGrammarScopes().includes(editor.getGrammar().scopeName);
+  activate() {
+    super.activate();
+
+    Object.getPrototypeOf(this._serverManager).getServer = function getServer(textEditor,{ shouldStart } = { shouldStart: false }) {
+      return __awaiter(this, void 0, void 0, function*() {
+        const finalProjectPath = textEditor.getPath();
+        if (finalProjectPath == null) {
+          // Files not yet saved have no path
+          return null;
+        }
+        const foundActiveServer = this._activeServers.find( s => finalProjectPath === s.projectPath);
+        if (foundActiveServer) {
+          return foundActiveServer;
+        }
+        const startingPromise = this._startingServerPromises.get(finalProjectPath);
+        if (startingPromise) {
+          return startingPromise;
+        }
+        return shouldStart && this._startForEditor(textEditor)
+          ? yield this.startServer(finalProjectPath)
+          : null;
+      });
+    }.bind(this._serverManager);
   }
 
+  getGrammarScopes() {
+    return ["source.dedukti"];
+  }
 
-  getGrammarScopes(){ return ["source.dedukti"]; };
+  getLanguageName() {
+    return "Dedukti";
+  }
 
-  getLanguageName(){ return "Dedukti";};
-
-  getServerName(){  return "lp-lsp"; };
+  getServerName() {
+    return "lp-lsp";
+  }
 
   uriToPath(uri) {
-      const url = URL.parse(uri);
-      if (url.protocol !== 'file:' || url.path === undefined) {
-          return uri;
+    const url = URL.parse(uri);
+    if (url.protocol !== "file:" || url.path === undefined) {
+      return uri;
+    }
+    let filePath = decodeURIComponent(url.path);
+    if (process.platform === "win32") {
+      // Deal with Windows drive names
+      if (filePath[0] === "/") {
+        filePath = filePath.substr(1);
       }
-      let filePath = decodeURIComponent(url.path);
-      if (process.platform === 'win32') {
-          // Deal with Windows drive names
-          if (filePath[0] === '/') {
-              filePath = filePath.substr(1);
-          }
-          return filePath.replace(/\//g, '\\');
-      }
-      return filePath;
+      return filePath.replace(/\//g, "\\");
+    }
+    return filePath;
   }
 
-  colorizebuffer(params){
-
+  colorizebuffer(params) {
     let path = this.uriToPath(params.uri);
     let i = 0;
     let z = 0;
@@ -87,87 +120,97 @@ class DeduktiLanguageClient extends AutoLanguageClient {
     let editor = "";
     let j = 0;
 
-    for(j=0;j<text_editors.length;j++){
+    for (j = 0; j < text_editors.length; j++) {
       let text_editor_path = text_editors[j].getPath();
-      if(text_editor_path == path ){
+      if (text_editor_path == path) {
         editor = text_editors[j];
       }
     }
 
     if (editor === "") {
       //console.log("l'éditeur n'a pas été trouvé.")
-    }
-    else{
-      let marker_color = editor.findMarkers({persistent:false});
+    } else {
+      let marker_color = editor.findMarkers({ persistent: false });
       //console.log("les markers :", marker_color);
-      for(z=0;z<marker_color.length;z++){
+      for (z = 0; z < marker_color.length; z++) {
         marker_color[z].destroy();
       }
     }
     // Then we put colors on those editors
-    for(i=0;i<params.diagnostics.length;i++){
-      if(params.diagnostics[i].message === "OK"){ // Hence Green
-        var marker = editor.markScreenRange(
-          [ [
-              params.diagnostics[i].range.start.line,
-              params.diagnostics[i].range.start.character
-            ],
-            [
-              params.diagnostics[i].range.end.line,
-              params.diagnostics[i].range.end.character
-            ]
+    for (i = 0; i < params.diagnostics.length; i++) {
+      if (params.diagnostics[i].message === "OK") {
+        // Hence Green
+        var marker = editor.markScreenRange([
+          [
+            params.diagnostics[i].range.start.line,
+            params.diagnostics[i].range.start.character
+          ],
+          [
+            params.diagnostics[i].range.end.line,
+            params.diagnostics[i].range.end.character
           ]
-        );
-        marker.setProperties({persistent:false, invalidate:'touch'}); //The color is diseappearing when 'touch'
-        let decoration = editor.decorateMarker(marker, {type: 'text', class:'Completed_lines'});
-      }
-      else { // Hence, in red
+        ]);
+        marker.setProperties({ persistent: false, invalidate: "touch" }); //The color is diseappearing when 'touch'
+        let decoration = editor.decorateMarker(marker, {
+          type: "text",
+          class: "Completed_lines"
+        });
+      } else {
+        // Hence, in red
         mydiagnostics.push(params.diagnostics[i]);
-        var marker = editor.markScreenRange(
-          [ [
-              params.diagnostics[i].range.start.line,
-              params.diagnostics[i].range.start.character
-            ],
-            [
-              params.diagnostics[i].range.end.line,
-              params.diagnostics[i].range.end.character
-            ]
+        var marker = editor.markScreenRange([
+          [
+            params.diagnostics[i].range.start.line,
+            params.diagnostics[i].range.start.character
+          ],
+          [
+            params.diagnostics[i].range.end.line,
+            params.diagnostics[i].range.end.character
           ]
-        );
-        marker.setProperties({persistent:false, invalidate:'touch'}); //The color disappears when 'touch'
-        let decoration = editor.decorateMarker(marker, {type: 'text', class:'Failed_line'});
+        ]);
+        marker.setProperties({ persistent: false, invalidate: "touch" }); //The color disappears when 'touch'
+        let decoration = editor.decorateMarker(marker, {
+          type: "text",
+          class: "Failed_line"
+        });
       }
     }
 
     return mydiagnostics;
-
   }
 
-  preInitialization(connection) { //Two new commands have been added or modified
+  preInitialization(connection) {
+    //Two new commands have been added or modified
     connection.onPublishDiagnostics = function(callback) {
-      let mycallback = function(params){
+      let mycallback = function(params) {
         //console.log(params.diagnostics);
-        //this.colorizebuffer(params);
         let mydiagnostics = this.colorizebuffer(params);
         params.diagnostics = mydiagnostics;
         callback(params);
-      }
-      connection._onNotification({ method: 'textDocument/publishDiagnostics' }, mycallback.bind(module.exports));
-    }
-
+      };
+      connection._onNotification(
+        { method: "textDocument/publishDiagnostics" },
+        mycallback.bind(module.exports)
+      );
+    };
+    /*
     this.connect_server = connection
     connection.onCustom("ProofAssistant/ActiveGoals",
     (e) => {
       this.updateView(e);
     });
-  };
+    */
+  }
 
-  startServerProcess () {
+  startServerProcess(projectPath) {
+    //await new Promise(resolve => atom.whenShellEnvironmentLoaded(resolve));
 
-    var command = atom.config.get("dedukti-editor.DeduktiSettings.lspServerPath");
+    var command = atom.config.get(
+      "dedukti-editor.DeduktiSettings.lspServerPath"
+    );
     var args = atom.config.get("dedukti-editor.DeduktiSettings.lspServerArgs");
 
-      /* Debug for developper (isma)
+    /* // Debug for developper (isma)
     var command_test = "./lplsp_test";
     const childProcess = child_process.spawn(command_test, args,{
       cwd: "/home/isma/Documents/dedukti-editor/src"
@@ -176,31 +219,31 @@ class DeduktiLanguageClient extends AutoLanguageClient {
 
     const childProcess = child_process.spawn(command, args);
     return childProcess;
-  };
+  }
 
   handleSpawnFailure(err) {
     //TODO: Use the `which` module to provide a better error in the case of a missing server.
-    atom.notifications.addError("Error starting the language server: " + atom.config.get("dedukti-editor.DeduktiSettings.lspServerPath"), {
-      dismissable: true,
-      description: "Please make sure you've followed the Installation section in the README and that the server is functional"
-    })
+    atom.notifications.addError(
+      "Error starting the language server: " +
+        atom.config.get("dedukti-editor.DeduktiSettings.lspServerPath"),
+      {
+        dismissable: true,
+        description:
+          "Please make sure you've followed the Installation section in the README and that the server is functional"
+      }
+    );
   }
 
-  apply_check_file (e) {}; //The first command launch this function
+  apply_check_file(e) {} //The first command launch this function
 
-  updateView(e){
-
+  updateView(e) {
     this.deduktiEditorView.updateSubProof(e);
-
-  }; //The second command launch this function
+  } //The second command launch this function
 
   //In case the first key binding is activated
-  command1(){ this.connect_server.didChangeTextDocument([]); };
-
-  command2(){ this.connect_server.sendCustomNotification("ProofAssistant/CapturedKey2",[]); };
-
-  command3(){ this.connect_server.sendCustomNotification("ProofAssistant/CapturedKey3",[]); };
-
+  //command1(){ this.connect_server.didChangeTextDocument([]); };
+  //command2(){ this.connect_server.sendCustomNotification("ProofAssistant/CapturedKey2",[]); };
+  //command3(){ this.connect_server.sendCustomNotification("ProofAssistant/CapturedKey3",[]); };
 }
 
 module.exports = new DeduktiLanguageClient();
